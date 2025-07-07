da03f70e6841f755584ac7ab26789723cadb6127
bankargaurav <gaurav.bankar@cloudywave.com>
2025-07-07 18:57:11 +0530

    allowed host

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    // Allows specific hosts
+    //allowedHosts: ['your-custom-domain.com', '.another-domain.com'], 
+    // Or, to allow any host (not recommended for production due to security risks)
+     allowedHosts: true, 
+  },
 })
